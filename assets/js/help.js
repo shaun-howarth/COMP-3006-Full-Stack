adeb1a7a8d6eb3fe909d2844d135e12a5e33eb21
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-12-21 12:51:15 +0000

    Have coded JS collapsible functionality on the help page.
    The frequently asked questions on this web page now has the collapsible feature whereby if a question button is clicked on the answer drop down will be activated. The "answer-content" CSS class containing the answer text will be toggled active in the list of next sibling elements after the "arrow-icon" class using the variable named "content" in the if statement .

---
+++ assets/js/help.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // variable made from button Class "drop-down" in help.ejs
+    let drop = document.getElementsByClassName("drop-down");
+
+    // Event listener included within for loop for the button to reveal the hidden answer text and turn arrow svg 90 degrees right usign toggle active in CSS "style" file
+    for (let i = 0; i < drop.length; i++) {
+        drop[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            this.getElementsByClassName("arrow-icon")[0].classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
+});
