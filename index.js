bdf79f3427aac13fb3c39a9e0bd5fb58421ef5c2
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-12-29 17:53:00 +0000

    Have made remaining app.get requests for web pages that will be made.
    The remaining web pages or ejs files to be made are: the to-do-list, login & sign-up pages.

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const server = app.listen(8080);
 app.set("view engine", "ejs");
 app.use("/assets", express.static("assets"));
 
-app.get("/", (req, res) => {
+app.get("/index", (req, res) => {
     res.render("index");
 });
 
 app.get("/help", (req, res) => {
     res.render("help");
+});
+
+app.get("/to-do-list", (req, res) => {
+    res.render("to-do-list");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/sign-up", (req, res) => {
+    res.render("sign-up");
 });
