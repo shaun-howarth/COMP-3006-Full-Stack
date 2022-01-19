319b80d9bde6b7e7392fb401797067647c3631e4
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2022-01-19 12:26:20 +0000

    Have code commented the following CSS and JS files.
    Have code commented all of the front-end CSS and JS files so far.

--- assets/js/help.js
+++ assets/js/help.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // variable made from button Class "drop-down" in help.ejs
     let drop = document.getElementsByClassName("drop-down");
 
-    // Event listener included within for loop for the button to reveal the hidden answer text and turn arrow svg 90 degrees right usign toggle active in CSS "style" file
+    // event listener included within for loop for the button to reveal the hidden answer text and turn arrow svg 90 degrees right usign toggle active in CSS "style" file
     for (let i = 0; i < drop.length; i++) {
         drop[i].addEventListener("click", function() {
             this.classList.toggle("active");
--- assets/js/index-page.js
+++ assets/js/index-page.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    // time greet function containing if statement for establishing greet message depending on date getHours JS object
     function timeGreeting() {
         let greeting = new Date();
         if (greeting.getHours() < 12) {
@@ -10,23 +11,25 @@ document.addEventListener("DOMContentLoaded", () => {
         return "Hello there & Good Evening!";
     }
 
+    // timeMessage function for getting "message" span id in HTML to display msg varaible value linking to timeGreeting fucntion
     function timeMessage () {
         let msg = timeGreeting();
         document.getElementById("message").innerHTML = msg;
     }
 
+    // showHour function for getting "hour-text" span id in HTML to display hour varaible value linking to timeGreeting fucntion
     function showHour () {
         let hour = new Date().getHours();
         document.getElementById("hour-text").innerHTML = "The time is: " + hour + ":";
     }
 
+    // showMinute function for getting "minute-text" span id in HTML to display minute varaible value linking to timeGreeting fucntion
     function showMinute () {
         let minute = new Date().getMinutes();
         document.getElementById("minute-text").innerHTML = "0" + minute;
     }
 
-    
-
+    // calling functions from above
     timeMessage();
     showHour();
     showMinute();
