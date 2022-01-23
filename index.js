92b0db087c23c815361a799353fd56e6bace2142
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2022-01-23 15:23:22 +0000

    Have moved around some configurations in "index.js" file.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const server = app.listen(8080);
 
-app.set("view engine", "ejs");
+
 app.use("/assets", express.static("assets"));
+
+app.set("View engine", "ejs");
 
 app.get("/index", (req, res) => {
     res.render("index");
@@ -13,14 +15,18 @@ app.get("/help", (req, res) => {
     res.render("help");
 });
 
-app.get("/to-do-list", (req, res) => {
-    res.render("to-do-list");
-});
-
 app.get("/login", (req, res) => {
     res.render("login");
 });
 
 app.get("/sign-up", (req, res) => {
     res.render("sign-up");
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello Test!');
+});
+
+app.get("/to-do-list", (req, res) => {
+    res.render("to-do-list");
 });
