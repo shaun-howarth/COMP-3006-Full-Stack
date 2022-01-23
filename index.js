d1ec4066e51aa3de032fe92dc8da686b7a5bb48d
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2022-01-23 17:08:23 +0000

    Have replaced res render quotations in app.get methods.
    Have replaced the double quotations with single quotation marks containing the ejs file name for the web pages in the get methods. This is so the ejs config engine would recognise the rendering of web pages as they were being quoted incorrectly.
    
    Have added an app.post body request method. Have added "app.use(express.urlencoded({ extended: true}));". This Urlencoded will allow the app to extract the data from the form by adding it to the body property of the request.
    
    Have also code commented areas.

--- index.js
+++ index.js
@@ -2,31 +2,38 @@ const express = require("express");
 const app = express();
 const server = app.listen(8080);
 
+// ejs view engine configuration for express app
+app.set("View engine", "ejs");
+
+app.use(express.urlencoded({ extended: true}));
 
 app.use("/assets", express.static("assets"));
 
-app.set("View engine", "ejs");
 
-app.get("/index", (req, res) => {
-    res.render("index");
-});
-
-app.get("/help", (req, res) => {
-    res.render("help");
-});
-
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-
-app.get("/sign-up", (req, res) => {
-    res.render("sign-up");
-});
-
+// index get render page
 app.get('/', (req, res) => {
-    res.send('Hello Test!');
+    res.render('index.ejs');
 });
 
-app.get("/to-do-list", (req, res) => {
-    res.render("to-do-list");
+// get methods for rendering web pages (ejs files)
+app.get('/help', (req, res) => {
+    res.render('help.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/sign-up', (req, res) => {
+    res.render('sign-up.ejs');
+});
+
+// get method for to-do-list page
+app.get('/to-do-list', (req, res) => {
+    res.render('to-do-list.ejs');
+});
+
+// post method
+app.post ('/',(req, res) => {
+    console.log(req.body);
 });
