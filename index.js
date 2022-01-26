5dfa1eba0f26db9c1e34deeb4d9ff86358bb5286
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2022-01-26 12:14:49 +0000

    Have made the CRUD update and delete data methods.
    These are the app route methods that deal with the update and delete of data for the tasks on the to-do-list web page. The TodoTask, "findByIdAndUpdate" & "findByIdAndDelete" objects are used to grab an id for a specific task from the mongo database. This is so a task can be updated with it's filled body text or simply deleted.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 // data models
 const TodoTask = require("./models/TodoTask");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.use("/assets", express.static("assets"));
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
     console.log("Connected to the database!");
+    // using TCP port: 3000 for node web app
     app.listen(3000, () => console.log("Server is running"));
     
 });
@@ -32,19 +34,19 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-// get methods for rendering web pages (ejs files)
+// help get render page
 app.get('/help', (req, res) => {
     res.render('help.ejs');
 });
 
-// get method for to-do-list page
+// get method for to-do-list page rendering: CRUD CREATE DATA
 app.get('/to-do-list', (req, res) => {
     TodoTask.find ({}, (err , tasks) => {
         res.render('to-do-list.ejs', { todoTask: tasks});
     });
 });
 
-// post method
+// post method: CRUD READ DATA
 app.post ('/',async (req, res) => {
     const todoTask = new TodoTask({
         content: req.body.content,
@@ -52,8 +54,35 @@ app.post ('/',async (req, res) => {
 
     try {
         await todoTask.save();
-        res.redirect("/");
+        res.redirect("/to-do-list");
     }   catch (err) {
-        res.redirect("/");
+        res.redirect("/to-do-list");
     }
+});
+
+// update method: CRUD UPDATE DATA
+app
+.route("/edit/:id")
+.get ((req, res) => {
+    const id = req.params.id;
+    TodoTask.find ({}, (err, tasks) => {
+        res.render("to-do-list-edit.ejs", { todoTask: tasks, idTask: id });
+    });
+})
+.post ((req, res) => {
+    const id = req.params.id;
+
+    TodoTask.findByIdAndUpdate(id, { content: req.body.content}, (err) => {
+        if (err) return res.send(500, err);
+        res.redirect("/to-do-list");
+    });
+});
+
+// delete method: CRUDE DELETE DATA: remove task card
+app.route("/delete/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndDelete(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/to-do-list");
+    });
 });
