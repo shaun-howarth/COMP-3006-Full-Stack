88258e33a1d42b8034331698c6d267bb59a2ab68
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2022-01-28 12:32:04 +0000

    script.js file created and set up for chat room.
    This JS file is used as the front-end js file for the chat room web page. It acts as the socket.io server for handling the web socket functionality between the chat room clients. Emit options and div elements from the chat web page are configured here.

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 const socket = io('http://localhost:8080')
+const messageContainer = document.getElementById('message-wrapper')
+const messageForm = document.getElementById('send-wrapper')
+const messageInput = document.getElementById('message-input')
+
+const username = prompt('Please enter your chat name!')
+appendMessage('You have joined the chat room.')
+socket.emit('new-user', username)
 
 socket.on('chat-message', data => {
-    console.log(data)
+    appendMessage(data.username + ": " + data.message)
 })
+
+socket.on('user-connected', username => {
+    appendMessage(username + " Connected");
+    
+})
+
+socket.on('user-disconnected', username => {
+    appendMessage(username + " Disconnected");
+    
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage("You: " + message);
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
